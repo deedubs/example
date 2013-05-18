536ef6af057d50b77af1ce8d6c71e025ba9e7ee1
Dan Williams <me@deedubs.com>
2013-05-18 00:55:30 -0400

    config example

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var http = require('http');
+var db = require('./lib/db');
+var config = require('./lib/config');
+var debug = require('debug')('app');
 
-http.createServer(function (req, res) {
+var server = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(process.env.PORT || 3000);
+  res.end('Hello World\n' + JSON.stringify(config.cfg));
+})
+
+config.ready(function (err, cfg) {
+  debug('Listening @ http://localhost:%d', cfg.PORT);
+  server.listen(cfg.PORT);
+});
